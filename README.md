288913a331e9be8f01f88ac341faa4ef83488b25
Jeff Akins <jeffakins21@gmail.com>
2021-10-13 17:34:04 -0700

    Updates to notebook

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ By Jeff Akins
 The goal of this project is to examine the current state of the EV charging network within the United States and to project the growth of the EV charging network through 2025. 
 ### Additional Goals:
 
-## Executive Summary
+## Executive Summary:
 
 ## How to Reproduce:
 - You will need a copy of the 'alternative fuel stations 2021 csv' file, which can be downloaded from data.gov [here](https://catalog.data.gov/dataset/alternative-fueling-station-locations-422f2/resource/341957d8-daf6-4a38-ab1d-8ec1bc21cfb9)
 - Clone my github repo
 - Run the 'individual_prject' notebook
-- Enjoy!
+- I hope that you find value in your exploration!
 
 
 ## Pipeline 
@@ -23,12 +23,48 @@ The goal of this project is to examine the current state of the EV charging netw
 [Trello Board](https://trello.com/b/nCuPtuTu/individual-project-plan)
 
 ### Acquire:
+You can find the original dataset file [here](https://catalog.data.gov/dataset/alternative-fueling-station-locations-422f2/resource/341957d8-daf6-4a38-ab1d-8ec1bc21cfb9) on data.gov
+I downloaded the CSV version of the file with the name: 'alternative-fuel-stations-2021-csv-3.csv'
+
+### Data Prep:
+The following steps were taken to clean the data:
+
+- Dropped all rows that were not "electric" charging stations
+- Dropped all columns with over 1000 null values
+- Dropped an additional 12 columns that would not be used for this project
+- Changed the pricing column to only include 'free' or 'cost' in the rows
+- Filled the EV connector types nulls with the most common connector type of 'J1772'
+- Fill EV Network nulls with most common 'non network' label
+- Added values for 2 missing longitude values based on city location
+- Renamed columns for ease of referencing and use with pandas
+- Converted date columns to pandas datetime type
+- Added a column for year that the charging station was opened
+- Converted all 'count' columns into 'int' type and filled nulls with 0
+
+Here is a summary of the data after I completed the cleaning process:
 
 #### Data dictionary
 |Index | Column Name | Description | Row Count | Dtype|
 |---|---|---|---|---|
+| 0  | fuel_code         | Type of Fuel i.e. 'Electric'                         | 47006 non-null | object       |
+| 1  | station_name      | Name of station                                      | 47006 non-null | object       |
+| 2  | address           | Address                                              | 47006 non-null | object       |
+| 3  | city              | City                                                 | 47006 non-null | object       |
+| 4  | state             | State                                                | 47006 non-null | object       |
+| 5  | zip               | Zip Code                                             | 47006 non-null | object       |
+| 6  | group_access_code | Whether public or private                            | 47006 non-null | object       |
+| 7  | level1_evse_num   | Count of level 1 chargers                            | 47006 non-null | int64        |
+| 8  | level2_evse_num   | Count of level 2 chargers                            | 47006 non-null | int64        |
+| 9  | ev_dc_fast_count  | Count of DC fast chargers                            | 47006 non-null | int64        |
+| 10 | ev_network        | Whether the charging stations is part of a network   | 47006 non-null | object       |
+| 11 | lat               | Latitude location                                    | 47006 non-null | float64      |
+| 12 | long              | Longitude location                                   | 47006 non-null | float64      |
+| 13 | update_date       | Date stations information was updated                | 47006 non-null | datetime64   |
+| 14 | open_date         | Date station was opened                              | 47006 non-null | datetime64   |
+| 15 | connector_type    | Type of charging connector                           | 47006 non-null | object       |
+| 16 | pricing           | Whether Free or Costs to charge                      | 47006 non-null | object       |
+| 17 | year_opened       | Year the station opened                              | 47006 non-null | int64        |
 
-### Data Prep:
 
 ### Explore:
 
--- wrangle.py
+++ wrangle.py
@@ -11,7 +11,7 @@ def clean_ev(csv_file_name):
 
     ev_21 = pd.read_csv(csv_file_name)     # Import csv file 
         
-    ev_21 = ev_21[ev_21['Fuel Type Code'] == 'ELEC']                    # To keep only EV stations
+    ev_21 = ev_21[ev_21['Fuel Type Code'] == 'ELEC']    # To keep only EV stations
         
     ev_21 = ev_21.dropna(axis=1, thresh=1000)   # Drop columns that mostly have nulls
                                                 # Thresh of 1000 was chosen because I wanted to keep the 'EV Level1 EVSE Num' column
@@ -19,10 +19,10 @@ def clean_ev(csv_file_name):
     # Additional columns to drop:
     columns_to_drop = ['Intersection Directions', 'Station Phone', 'Status Code', 'Access Days Time', 
                    'EV Network Web', 'Date Last Confirmed', 'Geocode Status', 'Groups With Access Code (French)',
-                   'Owner Type Code', 'Access Code', 'Access Detail Code', 'Facility Type']
+                   'Owner Type Code', 'Access Code', 'Access Detail Code', 'Facility Type', 'ID', 'Country']
     ev_21 = ev_21.drop(columns=columns_to_drop)
 
-    # Update cost of charginf to free or cost:
+    # Update cost of charging to free or cost:
     ev_21['EV Pricing'] = ev_21['EV Pricing'].apply(lambda x: 'Free' if (x == 'FREE') | (x == 'Free') else 'Cost')
     ev_21['EV Pricing'].value_counts()
 
@@ -50,11 +50,9 @@ def clean_ev(csv_file_name):
               'EV Network': 'ev_network',
               'Latitude': 'lat',
               'Longitude': 'long',
-              'ID': 'id',
               'Updated At': 'update_date',
               'Open Date': 'open_date',
               'EV Connector Types': 'connector_type',
-              'Country': 'country',
               'EV Pricing': 'pricing'}
 
     ev_21 = ev_21.rename(columns=rename_list)
@@ -79,7 +77,7 @@ def clean_ev(csv_file_name):
     ev_21.update_date = ev_21.update_date.dt.date
     ev_21.open_date = ev_21.open_date.dt.date
 
-    # Converting counts to int type and filling nulls with 0:
+    # Converting count columns into int type and filling nulls with 0:
     ev_21['level1_evse_num'] = ev_21['level1_evse_num'].fillna(0)
     ev_21['level2_evse_num'] = ev_21['level2_evse_num'].fillna(0)
     ev_21['ev_dc_fast_count'] = ev_21['ev_dc_fast_count'].fillna(0)
@@ -91,5 +89,28 @@ def clean_ev(csv_file_name):
     # Once again converting dates to datetime format:
     ev_21.update_date = pd.to_datetime(ev_21.update_date)
     ev_21.open_date = pd.to_datetime(ev_21.open_date)
+
+    # Replacing 'Public w/descriptor' with just 'Public' (will add a loop later to clean it up)
+    ev_21.group_access_code = ev_21.group_access_code.replace('Public - Call ahead', 'Public') 
+    ev_21.group_access_code = ev_21.group_access_code.replace('Public - Credit card at all times', 'Public')
+    ev_21.group_access_code = ev_21.group_access_code.replace('Public - Card key at all times', 'Public')
+    ev_21.group_access_code = ev_21.group_access_code.replace('TEMPORARILY UNAVAILABLE (Public)', 'Public')
+    ev_21.group_access_code = ev_21.group_access_code.replace('TEMPORARILY UNAVAILABLE (Public - Call ahead)', 'Public')
+    ev_21.group_access_code = ev_21.group_access_code.replace('TEMPORARILY UNAVAILABLE (Public - Credit card at all times)', 'Public')
+
+    # Replacing 'Private w/descriptor' with just 'Private'
+    ev_21.group_access_code = ev_21.group_access_code.replace('Private - Government only', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('Private - Credit card at all times', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('Private - Fleet customers only', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('Private - Call ahead', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('Private - Credit card after hours', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('TEMPORARILY UNAVAILABLE (Private - Government only)', 'Private')
+    ev_21.group_access_code = ev_21.group_access_code.replace('TEMPORARILY UNAVAILABLE (Private)', 'Private')
+
+    # Dropping stations that are 'planned' and not yet accessible:
+    ev_21 = ev_21[ev_21.group_access_code != 'PLANNED - not yet accessible (Public)']
+    ev_21 = ev_21[ev_21.group_access_code != 'PLANNED - not yet accessible (Public - Credit card at all times)']
+    ev_21 = ev_21[ev_21.group_access_code != 'PLANNED - not yet accessible (Public - Call ahead)']
+    ev_21 = ev_21[ev_21.group_access_code != 'PLANNED - not yet accessible (Private)']
 
     return ev_21
